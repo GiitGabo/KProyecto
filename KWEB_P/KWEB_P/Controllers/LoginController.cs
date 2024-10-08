f2b3e4d554d9bf53c3e06411e3fdbaea57a69d0e
Melvin Berrios <melvinberrios48@gmail.com>
2024-10-08 14:42:55 -0600

    Semana 3

---
+++ KWEB_P/KWEB_P/Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿using KWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult RegistroUsuario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegistroUsuario(Usuario model)
+        {
+
+            var comunes = new MetodosComunes();
+            var edad = comunes.CalcularEdad(1948);
+
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult InicioSesion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InicioSesion(Usuario model)
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult RecuperarAcceso()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RecuperarAcceso(Usuario model)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ KWEB_P/KWEB_P/Models/MetodosComunes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KWeb.Models
+{
+    public class MetodosComunes
+    {
+        //Métodos o funciones
+        public int CalcularEdad(int annoNacimiento)
+        {
+            return DateTime.Now.Year - annoNacimiento;
+        }
+    }
+}
